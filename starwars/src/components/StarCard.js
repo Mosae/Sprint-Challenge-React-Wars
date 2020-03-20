61f66a4e7652db80f8b06d49cc22a68840de4ad3
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-20 12:43:28 -0500

    Fixed col alignment

--- starwars/src/components/StarCard.js
+++ starwars/src/components/StarCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Card, CardHeader, Col, Badge } from 'reactstrap';
+import { Alert, Card, CardHeader, Col, Badge, Row } from 'reactstrap';
 
 const StarCard = ({ data, height, mass }) => {
 	const person = data;
@@ -9,19 +9,25 @@ const StarCard = ({ data, height, mass }) => {
 	console.log(starHeight);
 
 	return (
-		<Col xs="12" sm="6" md="4" xl="6">
-			<Card>
-				<CardHeader>
-					<h2>
-						<Alert color="primary">Name: {person} </Alert>
-					</h2>
-					<Alert color="secondary">Height: {starHeight} m</Alert>
-					<h4>
-						<Badge color="warning">Mass: {starMass}</Badge>
-					</h4>
-				</CardHeader>
-			</Card>
-		</Col>
+		<Row>
+			<Col
+				md={{ span: 6, offset: 3 }}
+				xs="12"
+				sm="6"
+				style={{ padding: '1rem', align: 'centered' }}>
+				<Card>
+					<CardHeader>
+						<h2>
+							<Alert color="primary">Name: {person} </Alert>
+						</h2>
+						<Alert color="secondary">Height: {starHeight} m</Alert>
+						<h4>
+							<Badge color="warning">Mass: {starMass}</Badge>
+						</h4>
+					</CardHeader>
+				</Card>
+			</Col>
+		</Row>
 	);
 };
 export default StarCard;
