231221a6b62aca1ce02aafd797dc1a0ef683df1f
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-20 11:50:58 -0500

    Names displaying

--- starwars/src/components/StarCard.js
+++ starwars/src/components/StarCard.js
@@ -4,6 +4,6 @@ const StarCard = ({ data }) => {
 	const person = data;
 	console.log(person);
 
-	return <div className="character"></div>;
+	return <div className="character">Name: {person}</div>;
 };
 export default StarCard;
