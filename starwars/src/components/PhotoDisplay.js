07dd2615e7ff0dad98de77399c6d4cb928007140
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-20 11:48:52 -0500

    Getting names back

--- starwars/src/components/PhotoDisplay.js
+++ starwars/src/components/PhotoDisplay.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import StarCard from './StarCard';
 
 export default function PhotoDisplay() {
-	const [image, setImage] = useState([]);
+	const [data, setData] = useState([]);
 	useEffect(() => {
 		axios
 			.get('https://swapi.co/api/people/')
 			.then(response => {
 				console.log(response.data.results);
-				setImage(response.data.results);
+				setData(response.data.results);
 			})
 			.catch(error => {
 				console.log('data was not returned', error);
 			});
 	}, []);
 
-	return <div className="photo"></div>;
+	if (data.length === 0) {
+		return <div>Loading...</div>;
+	} else {
+		return data.map(data => {
+			console.log(data);
+			return <StarCard data={data.name} />;
+		});
+	}
 }
---
+++ starwars/src/components/StarCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StarCard = ({ data }) => {
+	const person = data;
+	console.log(person);
+
+	return <div className="character"></div>;
+};
+export default StarCard;
