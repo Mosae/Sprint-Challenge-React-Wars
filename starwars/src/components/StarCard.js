2ccd20019a1a7f7850257174eabf7d5f56dc4787
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-20 12:25:01 -0500

    Styled using reactstrap. Added colors, Cols, alert, badges

--- starwars/src/components/StarCard.js
+++ starwars/src/components/StarCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert } from 'reactstrap';
+import { Alert, Card, CardHeader, Col, Badge } from 'reactstrap';
 
 const StarCard = ({ data, height, mass }) => {
 	const person = data;
@@ -9,11 +9,19 @@ const StarCard = ({ data, height, mass }) => {
 	console.log(starHeight);
 
 	return (
-		<div className="character">
-			<Alert color="primary">Name: {person} </Alert>
-			<Alert color="secondary">Height: {starHeight} m</Alert>
-			<Alert color="info">Mass: {starMass} m</Alert>
-		</div>
+		<Col xs="12" sm="6" md="4" xl="6">
+			<Card>
+				<CardHeader>
+					<h2>
+						<Alert color="primary">Name: {person} </Alert>
+					</h2>
+					<Alert color="secondary">Height: {starHeight} m</Alert>
+					<h4>
+						<Badge color="warning">Mass: {starMass}</Badge>
+					</h4>
+				</CardHeader>
+			</Card>
+		</Col>
 	);
 };
 export default StarCard;
